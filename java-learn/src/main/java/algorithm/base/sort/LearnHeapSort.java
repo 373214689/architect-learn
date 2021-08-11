4d0cd2c0f2049be0bf422ca0e1d401e0969db772
liuyang <373214689@qq.com>
2021-08-11 19:56:45 +0800

    v1.0.0.2 heap sort algorithm

--- java-learn/src/main/java/algorithm/base/sort/LearnHeapSort.java
+++ java-learn/src/main/java/algorithm/base/sort/LearnHeapSort.java
@@ -11,45 +11,67 @@ public class LearnHeapSort {
         System.out.println("heapSort Result: " + java.util.Arrays.toString(arr));
     }
 
-    private static void heapSort(int[] arr) {
-        int length = arr.length;
-        //从最后一个非叶子节点开始向上构造最大堆
-        for (int i = (length/2 - 1); i >= 0; i--) { //感谢 @让我发会呆 网友的提醒，此处应该为 i = (len/2 - 1)
-            maxHeapify(arr, i, length - 1);
-        }
-        //调整堆结构+交换堆顶元素与末尾元素
-        for (int i = arr.length - 1; i > 0; i--) {
-            //将堆顶元素与末尾元素进行交换
-            int temp = arr[i];
-            arr[i] = arr[0];
-            arr[0] = temp;
-            //重新对堆进行调整
-            maxHeapify(arr, 0, i);
-        }
-    }
+/**
+ * 堆排序
+ *
+ * @param arr
+ */
+private static void heapSort(int[] arr) {
+    int length = arr.length;
+    //从最后一个非叶子节点开始向上构造最大堆
+    buildMaxHeap(arr, length);
+    System.out.println("heapSort[init]: " + java.util.Arrays.toString(arr));
+    //2.循环将堆首位（最大值）与末位交换，然后在重新调整最大堆
+    while (length > 0) {
+        // 交换堆顶元素与末尾元素
+        int temp = arr[length - 1];
+        arr[length - 1] = arr[0];
+        arr[0] = temp;
+        System.out.println("heapSort[" + length + "]: " + java.util.Arrays.toString(arr));
+        length--;
+        adjustHeap(arr, 0, length);
 
-    private static void maxHeapify(int[] arr, int index, int size) {
-        // 最大堆
-        int left = 2*index;
-        int right = 2*index+1;
-        int pivot = index; // 初始化基准位置，相当于树型中的一个节点， left 表示是左侧节点，right 表示右侧节点
-        if(left <= size && arr[left] > arr[index]) {
-            pivot = left; // 左侧节点大于基准值，则左侧上移
-        }
-        if(right <= size && arr[right] > arr[pivot]){
-            pivot = right; // 右侧节点大于基准值，则右侧上移
-        }
-        if(pivot != index){
-            // 如果基准值发生变化，说明左右节点有比基准值大的节点，交换两者的值
-            int temp = arr[pivot];
-            arr[pivot] = arr[index];
-            arr[index] = temp;
-            // 进行递归
-            maxHeapify(arr, pivot, size);
-        }
-    }
 
-    
+    }
+}
+
+/**
+ * 建立最大堆
+ *
+ * @param arr
+ */
+public static void buildMaxHeap(int[] arr, int size) {
+    //从最后一个非叶子节点开始向上构造最大堆
+    for (int i = (size - 1)/2; i >= 0; i--) {
+        adjustHeap(arr, i, size);
+    }
+}
+
+/**
+ * 调整使之成为最大堆
+ *
+ * @param arr
+ * @param index
+ * @param size
+ */
+public static void adjustHeap(int[] arr, int index, int size) {
+    int maxIndex = index;
+    int left = index * 2; // 左子树
+    int right = index * 2 + 1; // 右子树
+    //如果有左子树，且左子树大于父节点，则将最大指针指向左子树
+    if (left < size && arr[left] > arr[maxIndex])
+        maxIndex = left;
+    //如果有右子树，且右子树大于父节点，则将最大指针指向右子树
+    if (right < size && arr[right] > arr[maxIndex])
+        maxIndex = right;
+    //如果父节点不是最大值，则将父节点与最大值交换，并且递归调整与父节点交换的位置。
+    if (maxIndex != index) {
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[index];
+        arr[index] = temp;
+        adjustHeap(arr, maxIndex, size);
+    }
+}
 }
 
 
